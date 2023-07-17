32d9c08ef122fdf1bfdc463f6815d941b2145a76
sojugood <tkddlrtla@naver.com>
2023-07-17 17:58:43 +0900

    07.17

---
+++ 07.17/python/test.py
@@ -0,0 +1,27 @@
+'''
+1번 데이터 -> 1번 컴퓨터 ... 10번 -> 10번 ... 11번 -> 1번 ...
+데이터 개수 -> a ** b
+
+입력
+테스트 케이스의 개수 T
+정수 a b
+.
+.
+.
+.
+
+출력
+각 테스트 케이스에 대해 마지막 데이터가 처리되는 컴퓨터 번호
+'''
+import sys
+totals = []
+T = int(input())
+
+for _ in range(T):
+    a, b = map(int, sys.stdin.readline().rstrip().split())
+    case = a ** b
+    computer_num = (case % 10)
+    totals.append(computer_num)
+
+for total in totals:
+    print(total)
