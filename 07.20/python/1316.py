69ec147debee0ff13716fd5536a4db500b583bb4
sojugood <tkddlrtla@naver.com>
2023-07-21 01:14:24 +0900

    07.20

--- 07.20/python/1316.py
+++ 07.20/python/1316.py
@@ -13,3 +13,17 @@ for i in range(N):
         result += 0
 
 print(result)
+
+''' 좋은 코드
+a=int(input())
+count=a
+for i in range(a):
+  b=input()
+  for j in range(0,len(b)-1):
+    if b[j]==b[j+1]:
+      pass
+    elif b[j] in b[j+1:]:
+      count-=1
+      break
+print(count)
+'''
