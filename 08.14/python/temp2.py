beb76d1d50db0aee38853754dfa8b11b991ce9a2
unknown <tkddlrtla@naver.com>
2023-08-14 18:00:11 +0900

    08.14

--- 08.14/python/temp2.py
+++ 08.14/python/temp2.py
@@ -1,2 +1,22 @@
+direction = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+
+def dfs(i, j, day):
+    day += 1
+    arr[i][j] = 2
+    for dx, dy in direction:
+        ni, nj = i + dx, j + dy
+        if 0 <= ni < M and 0 <= nj < N:
+            if arr[ni][nj] == 0 or arr[ni][nj] == 1:
+                dfs()
+
+
 M, N, H = map(int, input().split())
 
+arr = [list(map(int, input().split())) for _ in range(N)]
+
+d = 0
+
+for i in range(M):
+    for j in range(N):
+        if arr[i][j] == 1:
+            dfs(i, j, d)
