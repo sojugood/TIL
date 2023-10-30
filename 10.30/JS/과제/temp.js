76738a81b58e4d60ac371df634550ad6158f8500
sojugood <tkddlrtla@naver.com>
2023-10-30 23:55:50 +0900

    10.30

---
+++ 10.30/JS/과제/temp.js
@@ -0,0 +1,12 @@
+/**
+ * 이름, 나이 입력하면 합쳐서 문자로 반환하는 함수
+ * @param {string} name 이름 넣으셈 
+ * @param {number} age 나이 넣으셈
+ * @returns 두 개 합쳐서 문자로 반환해줌
+ * @version 1.25
+ */
+function hello(name, age) {
+    return name + age
+}
+
+hello(kim, 25)
